db2e83f1c5dfc184301caa2e68e43a036f3b3fd0
Leonardo Gonçalves <leonardo.goncalves@totvs.com.br>
2020-01-30 18:34:57 -0300

    ciclo de vida do componente, principais metodos e exemplos

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -16,6 +16,30 @@ class TechList extends Component {
     techs: []
   };
 
+  // principais métodos de ciclo de vida do componente
+
+  // Executado assim que o componente aparece em tela.
+  componentDidMount() {
+    const techs = localStorage.getItem('techs');
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) })
+    }
+  }
+
+  // Executado sempre que houver alterações nas props ou estado.
+  componentDidUpdate(prevProps, prevState) {
+    // recebe o dados anteriores de prevProps e prevState para futuras comparações.
+    // tem acesso aos this.props, this.state
+
+    if (prevState.techs != this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs))
+    }
+  }
+
+  // Executado quando o componente deixa de existir.
+  componentWillUnmount() {}
+
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
   };
